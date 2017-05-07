2f9efce517231c7d6b79a933c50ca4a4e616ba42
kmalamas <kyrmalamas@gmail.com>
2017-05-07 20:06:45 +0200

    Added class for Person. Corresponds to FullContactPerson

---
+++ FullContactClient/model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullContactClient.model
+{
+    public class Person
+    {
+
+            public string likelihood { get; set; }
+            public ContactInfo contactInfo { get; set; }
+            public IEnumerable<SocialProfile> socialProfiles { get; set; }
+
+
+
+
+        public void printInfo()
+            {
+                Console.WriteLine("Likelihood: " + likelihood);
+                contactInfo.print();
+                Console.WriteLine("Social Profiles: ");
+                foreach (SocialProfile p in socialProfiles)
+                {
+                    p.printProfile();
+                }
+            }
+        }
+    }
+
