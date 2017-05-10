164ed527e9b996ba786fb7349d862629d3abab25
kmalamas <kyrmalamas@gmail.com>
2017-05-10 09:56:19 +0200

    added basic email validation

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,7 @@ using FullContactClientCore;
 using FullContactClient;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp
 {
@@ -18,10 +19,18 @@ namespace ConsoleApp
 
         static async Task AsyncMain()
         {
+            bool validInput = true;
+            string email = string.Empty;
             Console.WriteLine("Please enter an email address");
-            string email = Console.ReadLine();
-            if (!string.IsNullOrEmpty(email)) //TODO add proper input validation for email
-            { 
+            while (validInput)
+            {
+               
+                email = Console.ReadLine();
+                if (!string.IsNullOrEmpty(email) && IsValidEmail(email)) 
+                    validInput = false;
+                else Console.WriteLine("Please enter a valid email address.");
+            }
+
             IFullContactApi fullContactApi = new FullContactAPI();
             var person = await fullContactApi.LookupPersonByEmailAsync(email);
                 if (person != null)  //if api returns no data for the email
@@ -30,10 +39,24 @@ namespace ConsoleApp
                 }
                 else Console.WriteLine("No data returned for this email address. Press Any key to exit.");
             Console.ReadLine();
-            }
+
            
         }
 
+       static  bool IsValidEmail(string email)
+        {
+            string pattern = null;
+            pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
 
+            if (Regex.IsMatch(email, pattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
